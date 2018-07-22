68d081f9db8f8f8cc5743e557b32207e09f26df2
joanna <jmgsese@gmail.com>
2018-07-22 16:41:59 -0500

    First commit

---
+++ ex3.py
@@ -0,0 +1,37 @@
+# prints a string
+print("I will now count my chickens:")
+
+# prints a string "Hens" followed by an integer
+# order of operations:
+# 25 + (30 / 6)
+# 25 + 5
+# 30
+print("Hens", 25.0 + 30.0 / 6.0)
+# 100 - (25 * 3 % 4)
+# 100 - (75 % 4)
+# 100 - 3
+# 97
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+print("Now I will count the eggs:")
+
+# 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - .25 + 6.0
+# 3.0 + 2.0 + 1.0 - 5.0 + 0 - .25 + 6.0
+# 6.75
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# 5 < -2
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)

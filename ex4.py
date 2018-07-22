01a17db56130884cb508307198711b1fda053de1
joanna <jmgsese@gmail.com>
2018-07-22 16:52:49 -0500

    Complete exercise 4"

---
+++ ex4.py
@@ -0,0 +1,23 @@
+cars = 100
+space_in_a_car = 4
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print("There are", cars, "cars available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We have to put about", average_passengers_per_car, "in each car.")
+
+# Traceback (most recent call last):
+#   File "ex4.py", line 8, in <module>
+#     average_passengers_per_car = car_pool_capacity / passenger
+# NameError: name 'car_pool_capacity' is not defined
+
+# NameError above because there is no 'car_pool_capacity' variable.
+# Replace with 'carpool_capacity' to run.

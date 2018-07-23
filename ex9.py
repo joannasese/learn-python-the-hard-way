d0203e7c829b1e07b534e22523c733dba6c03150
joanna <jmgsese@gmail.com>
2018-07-22 19:31:44 -0500

    Complete ex9.py

---
+++ ex9.py
@@ -0,0 +1,14 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# \n creates a linebreak
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print ("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# """ double-quotes appear to create linebreaks, too
+print("""
+There's something going on hereself.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")

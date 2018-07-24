2254dfef083cdcb0154996ef8a7800410881940b
joanna <jmgsese@gmail.com>
2018-07-24 01:14:42 -0500

    Add to ex10.py

--- ex10.py
+++ ex10.py
@@ -17,3 +17,23 @@ print(fat_cat)
 # \n creates a linebreak
 # \t creates an indentation
 # \ before \ will allow '\' to show
+
+# \apple
+print("1. \\apple")
+# apple'
+print("2. apple\'")
+# apple"
+print("3. apple\"")
+# apple with bell sound
+print("4. apple\a")
+# appl apple
+print("5. apple\b apple")
+print("6. apple\f")
+print("7. apple\n")
+
+print("8. apple\r")
+print("9. apple\t")
+print("10. apple\u6789")
+# print("11. apple\U23456788")
+print("12. apple\v")
+print("13. apple\ooo")

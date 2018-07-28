e28db5b57003c897918dd1cfb8a97538cbbfbe14
joanna <jmgsese@gmail.com>
2018-07-28 03:07:06 -0500

    Complete ex15.py

---
+++ ex15.py
@@ -0,0 +1,22 @@
+from sys import argv
+
+# set variables to argument
+# you'll need to pass in variables in command line
+script, filename = argv
+
+# opens filename
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+# prints out content of txt
+print(txt.read())
+txt.close()
+
+print("Type the filename again:")
+# file_again set to user input
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
+txt_again.close()

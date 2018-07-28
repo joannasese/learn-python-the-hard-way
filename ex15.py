15f864e875d481cd246b306a52d0bba75314e516
joanna <jmgsese@gmail.com>
2018-07-28 13:23:47 -0500

    Add notes to ex15.py

--- ex15.py
+++ ex15.py
@@ -10,12 +10,15 @@ txt = open(filename)
 print(f"Here's your file {filename}:")
 # prints out content of txt
 print(txt.read())
+# closes file
 txt.close()
 
 print("Type the filename again:")
-# file_again set to user input
+# file_again set to user input,
+# which will only work if the filename is entered exactly
 file_again = input("> ")
 
+# opens file (submitted through user input), set to variable 'txt_again'
 txt_again = open(file_again)
 
 print(txt_again.read())

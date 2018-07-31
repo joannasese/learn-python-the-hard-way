4fe205f17cb05a479a5c9caf0efa880471de0dea
joanna <jmgsese@gmail.com>
2018-07-31 00:58:00 -0500

    Add notes

--- ex20.py
+++ ex20.py
@@ -2,18 +2,23 @@ from sys import argv
 
 script, input_file = argv
 
+# function reads entire file ('f')
 def print_all(f):
     print(f.read())
 
+# goes to beginning of file
 def rewind(f):
     f.seek(0)
 
+# prints line number and content of that line
 def print_a_line(line_count, f):
     print(line_count, f.readline())
 
+
+# sets to open file (entered in terminal as command)
 current_file = open(input_file)
 
-print("First let's print the whole file:\n")
+print("First let's print the whole file:")
 
 print_all(current_file)
 
@@ -26,8 +31,10 @@ print("Let's print three lines:")
 current_line = 1
 print_a_line(current_line, current_file)
 
+# current_line = 2
 current_line += 1
 print_a_line(current_line, current_file)
 
+# current_line = 3
 current_line += 1
 print_a_line(current_line, current_file)
